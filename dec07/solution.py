1f0c9d94fc0c9764a114ab212dd758dcd6ffbd1f
Mason Egger <mason@masonegger.com>
2020-12-07 20:34:13 -0600

    adding day 7 part 2

--- dec07/solution.py
+++ dec07/solution.py
@@ -2,17 +2,17 @@ class BagNode:
     def __init__(self, name, count=0):
         self.name = name
         self.parents = []
-        self.children = []
-        self.count = 0
+        self.children = {}
+        self.count = count
 
-    def add_child(self, child):
-        self.children.append(child)
+    def add_child(self, name, count):
+        self.children[name] = count
 
     def add_parent(self, parent):
         self.parents.append(parent)
 
     def __str__(self):
-        return self.name + " " + str(self.count)
+        return self.name
 
 
 def generate_map(rules):
@@ -34,23 +34,30 @@ def generate_map(rules):
     return rulez
 
 
-def do_stuff(rules, bags_truth_table):
+def make_graph(rules, bags_truth_table):
     for rule in rules.keys():
         if rule in bags_truth_table:
             bag = bags_truth_table[rule]
             for name, count in rules[rule].items():
                 child = bags_truth_table.get(name, BagNode(name, count))
                 child.add_parent(bag)
-                bag.add_child(child)
+                bag.add_child(child, count)
                 bags_truth_table[name] = child
         else:
             new_bag_obj = BagNode(rule)
             for name, count in rules[rule].items():
                 child = bags_truth_table.get(name, BagNode(name, count))
                 child.add_parent(new_bag_obj)
-                new_bag_obj.add_child(child)
+                new_bag_obj.add_child(child, count)
                 bags_truth_table[name] = child
             bags_truth_table[rule] = new_bag_obj
+
+
+def get_num_bags(bag, num_current_bags):
+    answer = 0
+    for child, count in bag.children.items():
+        answer += get_num_bags(child, count)
+    return num_current_bags * answer + num_current_bags
 
 
 if __name__ == "__main__":
@@ -60,7 +67,7 @@ if __name__ == "__main__":
     rules = generate_map(rules)
     bags = {}
 
-    do_stuff(rules, bags)
+    make_graph(rules, bags)
 
     parents = bags["shiny gold"].parents
     answer_set = set()
@@ -72,3 +79,13 @@ if __name__ == "__main__":
         parents = parents[1:]
 
     print(len(answer_set))
+    print(get_num_bags(bags["shiny gold"], 1) - 1)
+
+"""     children = bags["shiny gold"].children
+    answer = bags["shiny gold"].count
+
+    while len(children) > 0:
+
+        if children[0].children != []:
+            children = children + children[0].children
+        children = children[1:] """
